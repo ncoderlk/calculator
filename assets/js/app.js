e0b577f6e6800db94a8b07df7563aa1b784e6355
ncoderlk <ncoderlk@gmail.com>
2022-05-20 10:00:06 +0530

    Fixed some bugs && updated

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,11 +1,11 @@
-
+//Button to One Variable
 const btn=document.querySelectorAll('button[id="btn"]');
 //input value
 function txtap(str) {
     var dis = document.getElementById("txt");
     dis.value += str;
 }
-//Answer
+//Proces Of Answer
 function outext() {
 
     var dis = document.getElementById("txt");
@@ -14,14 +14,40 @@ function outext() {
     dis.value = result;
     document.getElementById('trt').innerHTML=opt
 }
+//Erase Button
 function btnclear(){
+   
     var dis = document.getElementById("txt");
     var opt = dis.value;
-    var result = eval(opt.slice(0,-1));
-    dis.value = result;
+    var res="";
+    if(opt==""){
+       
+        dis.value=res
+    }
+    else if(opt==null){
+        
+        dis.value=res
+    }
+    else if(opt==undefined){
+       
+        dis.value=res
+    }
+    else if(opt=="undefined"){
+        dis.value=res
+    }
+    else{
+        var result = eval(opt.slice(0,-1));
+        dis.value = result;
+        
+    }
+   
 }
+//All Clear Button
 function btnaclear(){
+
     location.reload();
+    var dis=document.getElementById('txt');
+    return dis.value="";
 }
 //Process
 for (var i = 0; i < btn.length; i++) {
@@ -47,7 +73,4 @@ for (var i = 0; i < btn.length; i++) {
             break;
     }
 }
-
-function re(){
-    location.reload();
-}
+//Developed By NcoderLK
